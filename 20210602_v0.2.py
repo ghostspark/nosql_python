6a7897a46a2cde85988ca0919fe0e95da4ec6135
杜乐然 <65540762+ghostspark@users.noreply.github.com>
2021-06-02 00:45:20 +0800

    Create 20210602_v0.2.py
    允许循环使用该代码，但是仍有不足。
    接下来：
    1.拆分程序成多个（class方式）
    2.修改文档模式--尝试word

---
+++ 20210602_v0.2.py
@@ -0,0 +1,47 @@
+import redis
+import sys
+#pool=redis.ConnectionPool(host="124.71.227.41",port=6379)
+r = redis.Redis(host='127.0.0.1', port=6379)
+#r = redis.Redis(connection_pool=pool)
+print(r.flushdb())
+
+
+excludes = {"the","and","of","you","a","i","my","in"}#设定无效词
+def getText():
+    #txt = open("D:/DLR/code/PYTHON/6-行文代码codes/hamlet.txt", "r").read()
+    txt = open(input("请输入文章(txt)路径(例D:/hamlet.txt)："), "r").read()
+    txt = txt.lower()
+    for ch in '!"#$%&()*+,-./:;<=>?@[\\]^_‘{|}~':
+        txt = txt.replace(ch, " ")   #将文本中特殊字符替换为空格
+    return txt
+def dealText():
+    i='2'
+    j='2'
+    hamletTxt = getText()
+    words  = hamletTxt.split()
+    counts = {}
+    for word in words:			
+        counts[word] = counts.get(word,0) + 1
+    for word in excludes:#不统计无效词
+        del(counts[word])    
+    items = list(counts.items())
+    items.sort(key=lambda x:x[1], reverse=True) 
+    dict_tag=dict(items)
+
+    name_in=input("请输入存储名称：")
+    r.hmset(name_in, dict_tag)
+    while j!='0':
+        name_search=input("请输入所要查询的存储空间名称：")
+        search_tar=input("请输入所查询的关键词：")
+        j=name_search
+        print(r.hget(name_search,search_tar))
+    
+    ret=input("是否继续进行录入？（y/n）")
+    if ret=='y':
+        return dealText()
+    else:
+        print("thanks for using")
+    sys.exit()
+    
+dealText()
+
