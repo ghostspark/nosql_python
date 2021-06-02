43d8f3bbce620eddf82c9fc95265f4e3cf8f42c0
杜乐然 <65540762+ghostspark@users.noreply.github.com>
2021-06-02 20:25:00 +0800

    Update wordCount.py

--- wordCount.py
+++ wordCount.py
@@ -1,8 +1,10 @@
+import wordcount_2
 
 excludes = {"the","and","of","you","a","i","my","in"}#设定无效词
 def getText():
-    txt = open("D:/DLR/code/PYTHON/6-行文代码codes/hamlet.txt", "r").read()
-    txt = open(input("请输入文章(txt)路径(例D:/hamlet.txt)："), "r").read()
+    #txt = open("D:/DLR/code/PYTHON/6-行文代码codes/hamlet.txt", "r").read()
+    #txt = open(input("请输入文章(txt)路径(例D:/hamlet.txt)："), "r").read()
+    txt = open("tar.txt", "r").read()
     txt = txt.lower()
     for ch in '!"#$%&()*+,-./:;<=>?@[\\]^_‘{|}~':
         txt = txt.replace(ch, " ")   #将文本中特殊字符替换为空格
